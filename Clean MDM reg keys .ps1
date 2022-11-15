a6a7b6170917485058705a417e7eb6bec0806642
mmradio89 <118169784+mmradio89@users.noreply.github.com>
2022-11-15 08:43:12 +0100

    Add files via upload

---
+++ Clean MDM reg keys .ps1
@@ -0,0 +1,16 @@
+﻿$key = Get-Item -Path "HKLM:\SOFTWARE\Microsoft\Enrollments\"
+
+
+
+foreach ($keys in $key.GetSubKeyNames()) {
+    
+   
+    if($keys -notmatch "Ownership" -and $keys -notmatch "Context" -and $keys -notmatch "PollOnLoginTasksCreated" -and $keys -notmatch "Status" -and $keys -notmatch "ValidNodePaths") {
+
+        #Write-Host "Jeg sletter denne " $keys -ForegroundColor Red
+        Remove-Item -Path HKLM:\SOFTWARE\Microsoft\Enrollments\$keys -Force -Recurse
+    }
+}
+
+
+Gpupdate /force
